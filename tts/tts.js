59a081056e104804e65f23c73181b6521fc59874
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-03-28 09:54:41 +0100

    Fix to voice

--- tts/tts.js
+++ tts/tts.js
@@ -1,18 +1,18 @@
 /*
 *
-*  Using the text example.
+*  Command line driven TTS
+*
+*  Requires node-flite and flite
+*  Usage: node tts.js
 *
 */
 
-var flite = require('flite')
+var flite = require('flite');
 
-var message = "Hello. Welcome to the Auditory UX session"
-
+var message = "Hello. Welcome to the Auditory UX session";
 flite(function (err, speech) {
-
-  speech.config(voice => 'awb');
-
+  //speech.config.voice="awb";
   speech.say(message, function (err) {
-    if (err) console.error('I\'m afraid I can\'t do that, Dave', err);
+    if (err) console.error('I\ve lost my voice: ', err);
   });
 });
